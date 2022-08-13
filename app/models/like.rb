3f16cc7ebe7f51573c7ec090f74a361c7cf82b3e
jmoseka <jamylam3@gmail.com>
2022-08-13 18:12:37 +0300

    Fix linters

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,8 +1,8 @@
 class Like < ApplicationRecord
-    belongs_to :user
-    belongs_to :post
-  
-    def update_likes_counter
-      post.increment!(:likesCounter)
-    end
+  belongs_to :user
+  belongs_to :post
+
+  def update_likes_counter
+    post.increment!(:likesCounter)
   end
+end
