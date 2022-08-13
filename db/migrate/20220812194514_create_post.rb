444c32dffd6de8297f16b9de57fc517e6d38ee90
jmoseka <jamylam3@gmail.com>
2022-08-13 18:11:27 +0300

    Add method

--- db/migrate/20220812194514_create_post.rb
+++ db/migrate/20220812194514_create_post.rb
@@ -1,7 +1,7 @@
 class CreatePost < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.integer :authorId
+      t.integer :author_id
       t.string :title
       t.text :text
       t.integer :commentCounter
