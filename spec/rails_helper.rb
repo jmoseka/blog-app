0f5d8b8ebd6417741609d2a07a73fb3b333a166a
jmoseka <jamylam3@gmail.com>
2022-08-19 09:51:30 +0300

    Update railes_helper file

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -25,7 +25,7 @@ require 'rspec/rails'
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
 begin
-  ActiveRecord::Migration.maintain_test_schema!
+  # ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
