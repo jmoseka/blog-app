c75cd76a4333e66830e31037ac2a83c42f938082
jmoseka <jamylam3@gmail.com>
2022-08-20 01:35:53 +0300

    Add posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,5 +6,29 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+    @comment = Comment.new
+    @like = Like.new
+  end
+
+  def new
+    @post = Post.new
+    @params = params
+  end
+
+  def create
+    @post = Post.new(posts_params)
+    if @post.save
+      redirect_to user_posts_path(id: @post.id, user_id: @post.author_id)
+
+    else
+      render :new, status: :unprocessable_entity, content_type: 'text/html'
+      headers['Content-Type'] = 'text/html'
+    end
+  end
+
+  private
+
+  def posts_params
+    params.require(:post).permit(:title, :text, :author_id)
   end
 end
