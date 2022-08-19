61fb55ecde56aa88bf454890de73eea125ed6f40
jmoseka <jamylam3@gmail.com>
2022-08-19 22:40:52 +0300

    Update post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,10 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @user = User.find(params[:user_id])
+    @user_posts = Post.where(author_id: params[:user_id]).order(created_at: :desc)
+  end
 
-  def show; end
+  def show
+    @post = Post.find(params[:id])
+  end
 end
