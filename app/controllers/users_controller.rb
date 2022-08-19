3935a548f25fb1467d80760e21f5bedb9079b2c5
jmoseka <jamylam3@gmail.com>
2022-08-19 22:30:57 +0300

    Update user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @recent_post = Post.where(author_id: params[:id]).order(created_at: :desc).limit(3)
   end
 end
