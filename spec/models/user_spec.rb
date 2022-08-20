5d7faa1f18efb1e0d321c5a310ecd15997d7411b
jmoseka <jamylam3@gmail.com>
2022-08-20 16:29:17 +0300

    Add user model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validaiton tests for user' do
+    subject { User.new(name: 'Henry Kc', photo: 'profile.png', bio: 'Henry the great', posts_counter: 0) }
+    before { subject.save }
+
+    it 'Should have name present' do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'Should have post counter as an integer' do
+      subject.posts_counter = 3
+      expect(subject).to be_valid
+    end
+
+    it 'Should have post counter not as float' do
+      subject.posts_counter = 3.5
+      expect(subject).to_not be_valid
+    end
+
+    it 'Should have post counter not be less than zero' do
+      subject.posts_counter = -1
+      expect(subject).to_not be_valid
+    end
+
+    it 'Should have the subject as valid' do
+      expect(subject).to be_valid
+    end
+  end
 end
