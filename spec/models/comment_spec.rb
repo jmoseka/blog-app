fbfd93cd1781d5981ce676b0c7df01ff6b4e5702
jmoseka <jamylam3@gmail.com>
2022-08-20 16:35:34 +0300

    Add test for comments

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validation tests for Comments' do
+    subject { Comment.new(text: 'Comments creation', author_id: 2, post_id: 2) }
+
+    before { subject.save }
+
+    it 'Find author_id to be an integer' do
+      subject.author_id = 'henry'
+      expect(subject).to_not be_valid
+    end
+
+    it 'Find author_id to be an integer greater or equal to zero' do
+      subject.author_id = -2
+      expect(subject).to_not be_valid
+    end
+
+    it 'Find post_id to be an integer' do
+      subject.post_id = 'henry'
+      expect(subject).to_not be_valid
+    end
+
+    it 'Find post_id to be an integer greater or equal to zero' do
+      subject.post_id = -2
+      expect(subject).to_not be_valid
+    end
+  end
 end
