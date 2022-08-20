ba9ae122e4b2e8652daadcc15fd07e79dacd0137
jmoseka <jamylam3@gmail.com>
2022-08-20 16:35:05 +0300

    Add test for posts

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validaiton for post' do
+    subject do
+      jamila = User.new(name: 'Jamila Kc', photo: 'profile.png',
+                       bio: 'I am coming out to be the best computer programmer', posts_counter: 0)
+      Post.new(title: 'My best friend', text: 'He will be unvailed at the end of my program at Microverse',
+               comments_counter: 3, likes_counter: 3, author_id: jamila.id)
+    end
+
+    before { subject.save }
+
+    it 'Should have title present' do
+      subject.title = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'Should have title not being too long' do
+      subject.title = 'one' * 200
+      expect(subject).to_not be_valid
+    end
+
+    it 'Should have comments counter as an integer' do
+      subject.comments_counter = 0.033
+      expect(subject).to_not be_valid
+    end
+
+    it 'Should have comments counter not be less than zero' do
+      subject.comments_counter = -1
+      expect(subject).to_not be_valid
+    end
+
+    it 'Should have likes counter not be less than zero' do
+      subject.likes_counter = -1
+      expect(subject).to_not be_valid
+    end
+  end
+
+  describe 'Test for last recent posts' do
+    subject { User.new(name: 'Jamila Kc', photo: 'profile.png', bio: 'Jamila the great', posts_counter: 0) }
+    before { subject.save }
+    it 'Should have maximum of five comments' do
+      expect(subject.last_recent_posts).to eq(subject.last_recent_posts)
+    end
+  end
 end
